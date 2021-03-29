5e55c33ec2429a6cf8289de708427dcc3581b3c3
Evgenii Shumeiko <jack.shumeiko@ya.ru>
2021-03-30 04:12:10 +0500

    add modal footer buttons

--- src/Components/Cart.vue
+++ src/Components/Cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-modal id="cart" title="Корзина">
+  <b-modal id="cart" title="Корзина" cancel-title="Очистить корзину" @cancel="clearCart" ok-title="Оформить заказ" ok-variant="success">
     <b-row>
       <CartItem v-for="item in data" :key="item.id" :item="item"></CartItem>
     </b-row>
@@ -21,6 +21,12 @@ export default {
         {id: 1, image: "https://picsum.photos/500?random=1", name: "Intelligent Granite Fish", price: 89},
         {id: 2, image: "https://picsum.photos/500?random=2", name: "Sleek Steel Sausages", price: 95}
       ]
+    }
+  },
+  methods: {
+    clearCart(bvModalEvt) {
+      bvModalEvt.preventDefault();
+      this.data = []
     }
   },
 
