68005b2e1021d027b7ce57780fc62581eeb2d8d1
Evgenii Shumeiko <jack.shumeiko@ya.ru>
2021-03-30 04:00:43 +0500

    Add infinity load

--- src/Components/Catalog.vue
+++ src/Components/Catalog.vue
@@ -1,9 +1,19 @@
 <template>
-  <b-row cols-sm="2" cols-md="3" cols-lg="4" cols="1" class="g-3">
-    <b-col v-for="item in data" :key="item.id">
-      <CatalogItem :key="item.id" :image="item.image" :name="item.name" :price="item.price"/>
-    </b-col>
-  </b-row>
+  <div>
+    <b-row cols-sm="2" cols-md="3" cols-lg="4" cols="1" class="g-3">
+      <b-col v-for="item in data" :key="item.id">
+        <CatalogItem :key="item.id" :image="item.image" :name="item.name" :price="item.price"/>
+      </b-col>
+    </b-row>
+    <b-row col="12" class="m-3">
+      <b-col>
+        <infinite-loading v-if="data.length" spinner="spiral"  @infinite="_infiniteScroll">
+          <div slot="no-more">Нууу, это точно Дно</div>
+        </infinite-loading>
+      </b-col>
+    </b-row>
+  </div>
+
 </template>
 
 <script>
@@ -13,25 +23,39 @@ export default {
   name: "Catalog",
   data() {
     return {
-      data: []
+      data: [],
+      offset: 1
     }
   },
   components: {
     CatalogItem
   },
   methods: {
-    _fetchData(limit = 1) {
-      return fetch(`/database/data${limit}.json`)
+    async _fetchData(offset = 1) {
+      return fetch(`/database/data${offset}.json`)
           .then((data) => {
             return data.json();
           })
           .then((data) => {
-            console.log(data.data)
             return data.data;
           })
+    },
+    _infiniteScroll($state) {
+      let fetchPromise = this._fetchData(++this.offset)
+      fetchPromise
+          .then((data) => {
+            this.data = [...this.data, ...data];
+            $state.loaded()
+          })
+          .catch((err) => {
+            console.warn('Something went wrong, when list tried init', err)
+            $state.complete()
+          })
+
+      return true;
     }
   },
-  beforeMount() {
+  created() {
     this._fetchData().then((data) => {
       this.data = data
     })
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import InfiniteLoading from "vue-infinite-loading";
 
 import './styles/style.sass';
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.component("infinite-loading", InfiniteLoading);
+
 
 new Vue({
     el: 'main',
